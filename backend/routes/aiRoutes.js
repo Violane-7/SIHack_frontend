754407ddcb9da5a33d14d4e2b5a6cb0842eab225
Violane-7 <smeraldo223@gmail.com>
2025-09-30 09:52:38 +0530

    v1

--- backend/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { proxyToAi } = require('../controllers/aiController');
-const auth = require('../middleware/authMiddleware');
-
-// Protect AI route if you want only authenticated users to use it
-router.post('/', auth, proxyToAi);
-
-module.exports = router;
